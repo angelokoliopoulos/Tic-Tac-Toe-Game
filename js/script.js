055d40db2df3d72e2ccbfefab73a2f636286848b
unknown <koliosor@gmail.com>
2023-06-02 11:33:43 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-const MessageDisplay = (() => {})();
+const MessageDisplay = (() => {
+  const heading = document.querySelector("h2");
 
-const createPlayer = (name, mark) => {
-  return {
-    name,
-    mark,
+  const winMsg = (player) => {
+    heading.innerText = `Player ${player} won the game!`;
   };
-};
+
+  const displayTurn = (player) => {
+    heading.innerText = `Player's ${player} turn`;
+  };
+
+  const tieMsg = () => {
+    heading.innerText = `It's a tie!`;
+  };
+
+  const clearDisplay = () => {
+    heading.innerText = "";
+  };
+  return {
+    winMsg,
+    displayTurn,
+    tieMsg,
+    clearDisplay,
+  };
+})();
+
 const GameBoard = (() => {
   const boxes = Array.from(document.querySelectorAll(".box"));
   const boardValues = ["", "", "", "", "", "", "", "", ""];
@@ -45,11 +63,18 @@ const GameBoard = (() => {
     }
   };
 
+  const removeListeners = () => {
+    boxes.forEach((box) =>
+      box.removeEventListener("click", Gamecontroller.handleClick)
+    );
+  };
+
   return {
     drawBoard,
     updateBoard,
     getBoardState,
     resetBoardValues,
+    removeListeners,
   };
 })();
 
@@ -58,8 +83,16 @@ const Gamecontroller = (() => {
   let players;
   let currentPlayer;
 
+  const createPlayer = (name, mark) => {
+    return {
+      name,
+      mark,
+    };
+  };
+
   const loadGame = () => {
     GameBoard.resetBoardValues();
+    MessageDisplay.clearDisplay();
     gameOver = false;
     players = [createPlayer("Player1", "O"), createPlayer("Player2", "X")];
     currentPlayer = 0;
@@ -69,8 +102,56 @@ const Gamecontroller = (() => {
   const handleClick = (e) => {
     let clickedArea = e.target.id;
     if (GameBoard.getBoardState()[clickedArea] != "") return;
-    console.log(e.target.id);
+
+    MessageDisplay.displayTurn(players[currentPlayer === 0 ? 1 : 0].mark);
     GameBoard.updateBoard(players[currentPlayer].mark, clickedArea);
+
+    if (checkForWin(GameBoard.getBoardState())) {
+      MessageDisplay.winMsg(players[currentPlayer].mark);
+      GameBoard.removeListeners();
+      return;
+    }
+
+    if (checkTie(GameBoard.getBoardState())) {
+      MessageDisplay.tieMsg();
+      GameBoard.removeListeners();
+      return;
+    }
+
+    swapPlayer();
+  };
+
+  const swapPlayer = () => {
+    currentPlayer = currentPlayer === 1 ? 0 : 1;
+  };
+
+  const checkTie = (gameboard) => {
+    return gameboard.every((cell) => cell !== "");
+  };
+
+  const checkForWin = (gameboard) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winConditions.length; i++) {
+      let [a, b, c] = winConditions[i];
+      if (
+        gameboard[a] &&
+        gameboard[a] === gameboard[b] &&
+        gameboard[b] === gameboard[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return {
